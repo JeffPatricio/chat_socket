337efc666db500badfd7064a54d32d49338dab33
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-02-20 10:20:45 -0300

    implementação do chat

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, '..', 'public ')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+  res.render('index.html')
+});
+
+const messages = [];
+
+io.on('connection', socket => {
+  console.log('socket conectado: ', socket.id);
+
+  socket.emit('previousMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data);
+    socket.broadcast.emit('receivedMessage', data);
+  })
+
+})
+
+server.listen(3030, () => console.log('server running in port 3030'));
